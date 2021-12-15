2b1ea578612046481a57680d4d7b42565d58e600
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-12-15 23:16:16 +0330

    test lexer

---
+++ MainPrj.py
@@ -0,0 +1,19 @@
+from lexer import TokenRules
+
+if __name__ == '__main__' :
+    a = TokenRules()
+    a.build()
+    data = '''
+        // FOR STATE
+        for(int i=0; i<=10 ; i++){
+        counter++;
+        if(counter == i){
+            counter = (i < 6) ? 0 : 1 ;
+        }else
+            counter >> 2;
+        }
+        /*
+            jast for check
+        */   
+    '''
+    a.main(data)

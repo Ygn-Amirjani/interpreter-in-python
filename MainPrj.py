2637f5a21b1c21667c500cf3d1c9b168e9fe754f
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-12-26 16:56:09 +0330

    half :(

--- MainPrj.py
+++ MainPrj.py
@@ -6,11 +6,11 @@ if __name__ == '__main__' :
     data = '''
         // FOR STATE
         for(int i=0; i<=10 ; i++){
-        counter++;
-        if(counter == i){
-            counter = (i < 6) ? 0 : 1 ;
-        }else
-            counter >> 2;
+            counter++;
+            if(counter == i){
+                counter = (i < 6) ? 0 : 1 ;
+            }else
+                counter += 2;
         }
         /*
             jast for check

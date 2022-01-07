8747ee59bb8506405182430ad449e8a7bde74ea9
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2022-01-07 18:22:08 +0330

    Completed

--- MainPrj.py
+++ MainPrj.py
@@ -1,14 +1,12 @@
 from lexer import TokenRules
 
 if __name__ == '__main__' :
-    a = TokenRules()
-    a.build()
     data = '''
         // FOR STATE
-        for(int i=0; i<=10 ; i++){
-            counter++;
+        for(int i=0; i<10 ; i+=1){
+            counter = i;
             if(counter == i){
-                counter = (i < 6) ? 0 : 1 ;
+                counter += 1 ;
             }else
                 counter += 2;
         }
@@ -16,4 +14,5 @@ if __name__ == '__main__' :
             jast for check
         */   
     '''
-    a.main(data)
+    a = TokenRules(data)
+    a.print_token()

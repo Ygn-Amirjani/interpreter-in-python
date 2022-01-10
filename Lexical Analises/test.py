bb470cad3d447d5c6bedd8a2241e3031b873bd60
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2022-01-10 10:34:40 +0330

    Completed

--- Lexical Analises/test.py
+++ Lexical Analises/test.py
@@ -2,14 +2,13 @@ from Token import TokenType
 from Lexing import Lexing
 
 def main() -> None :
-    sorce_code = 'int x = 3 + 42 * (s - t) ;'
+    sorce_code = 'for(int i===0; i<10; i+=1) { }'
     go_to_lexer = Lexing(sorce_code)
     find_token = go_to_lexer.NextToken()
 
     while find_token.tokenType != TokenType.EOF :
         print(find_token)
         find_token = go_to_lexer.NextToken()
-
 
 if __name__ == '__main__' :
     main()

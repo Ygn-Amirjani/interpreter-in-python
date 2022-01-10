a6e6979c7ac99147ede8975033eff5299e0aabb6
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2022-01-10 10:35:05 +0330

    Completed

--- Parsing.py
+++ Parsing.py
@@ -1,44 +1,38 @@
-from ply.yacc import YaccProduction as YP
+from ply.yacc import YaccProduction 
 from lexer import TokenRules
 import ply.yacc
 
 tokens = TokenRules.tokens
 
-
-def p_start(token : YP):
+def p_start(token : YaccProduction) :
     """start : nested_stmnt
              | variable_starter"""
     pass
 
-
-def p_variable_detector(token : YP):
+def p_variable_detector(token : YaccProduction):
     """variable_detector : CHAR variable_detector
                          | INT variable_detector
                          | CHAR
                          | INT"""
     pass
 
-
-def p_assign_detector(token : YP):
+def p_assign_detector(token : YaccProduction):
     """assign_detector : identifier_punctuator
                        | identifier_punctuator ASSIGN starting_brace"""
     pass
 
-
-def p_variable_starter(token : YP):
+def p_variable_starter(token : YaccProduction):
     """variable_starter : variable_detector assign_detector SEMICOLON
                         | variable_detector assign_detector COMMA assign_detector SEMICOLON
                         | variable_detector SEMICOLON"""
     pass
 
-
-def p_variable_starter_chains(token : YP):
+def p_variable_starter_chains(token : YaccProduction):
     """variable_starter_chains : variable_starter
                                | variable_starter_chains variable_starter"""
     pass
 
-
-def p_identifier_punctuator(token : YP):
+def p_identifier_punctuator(token : YaccProduction):
     """identifier_punctuator : IDENTIFIER
                              | LPAREN identifier_punctuator RPAREN
                              | identifier_punctuator LPAREN parameter_variable_starter RPAREN
@@ -47,15 +41,13 @@ def p_identifier_punctuator(token : YP):
                              | identifier_punctuator LPAREN IDENTIFIER COMMA IDENTIFIER RPAREN"""
     pass
 
-
-def p_parameter_variable_starter(token : YP):
+def p_parameter_variable_starter(token : YaccProduction):
     """parameter_variable_starter : variable_detector identifier_punctuator
                                   | variable_detector identifier_punctuator_2
                                   | variable_detector empty"""
     pass
 
-
-def p_starting_brace(token : YP):
+def p_starting_brace(token : YaccProduction):
     """starting_brace : assigning
                       | LBRACE starting_brace RBRACE
                       | LBRACE starting_brace COMMA starting_brace RBRACE
@@ -63,14 +55,12 @@ def p_starting_brace(token : YP):
                       | LBRACE starting_brace COMMA starting_brace COMMA RBRACE"""
     pass
 
-
-def p_variable_detector_2(token : YP):
+def p_variable_detector_2(token : YaccProduction):
     """variable_detector_2 : variable_detector identifier_punctuator_2
                            | variable_detector empty"""
     pass
 
-
-def p_identifier_punctuator_2(token : YP):
+def p_identifier_punctuator_2(token : YaccProduction):
     """identifier_punctuator_2 : LPAREN identifier_punctuator_2 RPAREN
                                | identifier_punctuator_2 LPAREN parameter_variable_starter RPAREN
                                | identifier_punctuator_2 LPAREN parameter_variable_starter COMMA parameter_variable_starter RPAREN
@@ -80,69 +70,59 @@ def p_identifier_punctuator_2(token : YP):
                                | LPAREN empty RPAREN"""
     pass
 
-
-def p_stmnt(token : YP):
+def p_stmnt(token : YaccProduction):
     '''stmnt :   expression_2 SEMICOLON
              | nested_stmnt
              | if
              | loops'''
     pass
 
-
-def p_nested_stmnt(token : YP):
+def p_nested_stmnt(token : YaccProduction):
     """nested_stmnt : LBRACE variable_starter_chains stmnt_chains RBRACE
                     | LBRACE stmnt_chains RBRACE
                     | LBRACE variable_starter_chains RBRACE
                     | LBRACE RBRACE"""
     pass
 
-
-def p_stmnt_chains(token : YP):
+def p_stmnt_chains(token : YaccProduction):
     """stmnt_chains : stmnt
                     | stmnt_chains stmnt"""
     pass
 
-
-def p_if(token : YP):
+def p_if(token : YaccProduction):
     """if : IF LPAREN expression RPAREN stmnt
           | IF LPAREN expression RPAREN stmnt ELSE stmnt"""
     pass
 
-
-def p_loops(token : YP):
+def p_loops(token : YaccProduction):
     """loops : WHILE LPAREN expression RPAREN stmnt
              | FOR LPAREN expression_2 SEMICOLON expression_2 SEMICOLON expression_2 RPAREN stmnt
              | DO stmnt WHILE LPAREN expression RPAREN SEMICOLON"""
     pass
 
-
-def p_expression_2(token : YP):
+def p_expression_2(token : YaccProduction):
     """expression_2 : empty
                     | expression"""
     pass
 
-
-def p_expression(token : YP):
+def p_expression(token : YaccProduction):
     """expression : assigning
                   | expression COMMA assigning"""
     pass
 
-
-def p_assigning(token : YP):
+def p_assigning(token : YaccProduction):
     """assigning : equal_unequal
                  | single_operand ASSIGN assigning
                  | single_operand PLUSASSIGN assigning"""
     pass
 
-
-def p_equal_unequal(token : YP):
+def p_equal_unequal(token : YaccProduction):
     """equal_unequal : comparison
                      | equal_unequal EQ comparison
                      | equal_unequal NOT_EQ comparison"""
     pass
 
-
-def p_comparison(token : YP):
+def p_comparison(token : YaccProduction):
     """comparison : arithmetic_precedence
                   | comparison LT arithmetic_precedence
                   | comparison GT arithmetic_precedence
@@ -150,36 +130,31 @@ def p_comparison(token : YP):
                   | comparison GE arithmetic_precedence"""
     pass
 
-
-def p_multiply_division(token : YP):
+def p_multiply_division(token : YaccProduction):
     """multiply_division : cast
                          | multiply_division TIMES cast
                          | multiply_division DIVIDE cast"""
     pass
 
-
-def p_cast(token : YP):
+def p_cast(token : YaccProduction):
     """cast : single_operand
             | LPAREN variable_detector_2 RPAREN cast"""
     pass
 
-
-def p_arithmetic_precedence(token : YP):
+def p_arithmetic_precedence(token : YaccProduction):
     """arithmetic_precedence : multiply_division
                              | arithmetic_precedence PLUS multiply_division
                              | arithmetic_precedence MINUS multiply_division"""
     pass
 
-
-def p_single_operand(token : YP):
+def p_single_operand(token : YaccProduction):
     """single_operand : expression_combination
                       | TIMES cast
                       | PLUS cast
                       | MINUS cast"""
     pass
 
-
-def p_expression_combination(token : YP):
+def p_expression_combination(token : YaccProduction):
     """expression_combination : IDENTIFIER
                               | NUMBER
                               | LPAREN expression RPAREN
@@ -188,11 +163,9 @@ def p_expression_combination(token : YP):
                               | expression_combination LPAREN RPAREN"""
     pass
 
-
-def p_empty(token : YP):
+def p_empty(token : YaccProduction):
     """empty : """
     pass
-
 
 data = '''
 {
@@ -202,7 +175,6 @@ data = '''
         i=i+1;
 }
 '''
-
 
 parser = ply.yacc.yacc()
 lexer = TokenRules(data)
